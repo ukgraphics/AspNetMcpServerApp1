0dfa42ac5730c6abd8390218587e8c8fa2ebc31e
Kuni <kuni.ujiie@mescius.com>
2025-05-21 11:55:24 +0900

    MCPツールを更新

--- AspNetMcpServerApp1/Program.cs
+++ AspNetMcpServerApp1/Program.cs
@@ -15,42 +15,18 @@ var app = builder.Build();
 app.MapMcp();
 app.Run();
 
-//[McpServerToolType]
-//public static class TimeTools
-//{
-//    [McpServerTool, Description("現在の時刻を取得")]
-//    public static string GetCurrentTime()
-//    {
-//        return DateTimeOffset.Now.ToString();
-//    }
-
-//    [McpServerTool, Description("指定されたタイムゾーンの現在の時刻を取得")]
-//    public static string GetTimeInTimezone(string timezone)
-//    {
-//        try
-//        {
-//            var tz = TimeZoneInfo.FindSystemTimeZoneById(timezone);
-//            return TimeZoneInfo.ConvertTime(DateTimeOffset.Now, tz).ToString();
-//        }
-//        catch
-//        {
-//            return "無効なタイムゾーンが指定されています";
-//        }
-//    }
-//}
-
 
 [McpServerToolType]
 public static class WeatherForecastTool
 {
-    [McpServerTool, Description("今日の仙台の天気予報を取得")]
-    public static async Task<string> GetWeatherForecastToday(HttpClient client)
+    [McpServerTool, Description("天気予報を取得")]
+    public static async Task<string> GetWeatherForecastCity(HttpClient client, string citycode)
     {
         // User-Agentを追加
         client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("aspnet-mcp-server-test", "1.0"));
 
         // 天気予報API（https://weather.tsukumijima.net/）にHTTP GETリクエストを送信
-        HttpResponseMessage response = await client.GetAsync("https://weather.tsukumijima.net/api/forecast?city=040010");
+        HttpResponseMessage response = await client.GetAsync("https://weather.tsukumijima.net/api/forecast?city=" + citycode);
 
         // レスポンスを確認
         response.EnsureSuccessStatusCode();
@@ -59,14 +35,16 @@ public static class WeatherForecastTool
 
         // 天気予報を取得
         using JsonDocument jsonDocument = await JsonDocument.ParseAsync(content);
-        string weatherforecasttoday = jsonDocument.RootElement
+
+        string title = jsonDocument.RootElement
+            .GetProperty("title")
+            .GetString() ?? string.Empty; // Nullチェック
+
+        string weatherforecast = jsonDocument.RootElement
             .GetProperty("description")
             .GetProperty("bodyText")
             .GetString() ?? string.Empty; // Nullチェック
 
-        return $"本日の仙台の天気予報：{weatherforecasttoday}";
+        return $"{title}：{weatherforecast}";
     }
-
-
-
 }
