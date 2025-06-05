9d930bda8822ae760648182f86b758873a6f5d1d
Kuni <kuni.ujiie@mescius.com>
2025-06-05 17:44:14 +0900

    MCPツールを更新

--- AspNetMcpServerApp1/Program.cs
+++ AspNetMcpServerApp1/Program.cs
@@ -1,11 +1,12 @@
 using ModelContextProtocol.Server;
 using System.ComponentModel;
+using System.Net.Http;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMcpServer()
-    .WithHttpTransport()
-    .WithToolsFromAssembly();
+   .WithHttpTransport()
+   .WithToolsFromAssembly();
 
 builder.Services.AddHttpClient();
 
@@ -13,25 +14,26 @@ var app = builder.Build();
 app.MapMcp();
 app.Run();
 
-
 [McpServerToolType]
 public static class WeatherForecastTool
 {
     [McpServerTool, Description("天気予報を取得")]
     public static async Task<string> GetWeatherForecastCity(
-        HttpClient client,
+        IHttpClientFactory httpClientFactory,
         [Description("地域別に定義されたID番号（https://weather.tsukumijima.net/primary_area.xml）")] string citycode)
     {
-        // 天気予報API（https://weather.tsukumijima.net/）にHTTP GETリクエストを送信
+        HttpClient client = httpClientFactory.CreateClient();
+
+        // 天気予報API（https://weather.tsukumijima.net/）にHTTP GETリクエストを送信  
         using HttpResponseMessage response = await client.GetAsync($"https://weather.tsukumijima.net/api/forecast?city={citycode}");
 
-        // レスポンスを確認
+        // レスポンスを確認  
         response.EnsureSuccessStatusCode();
 
-        // レスポンスの内容をストリームとして取得
+        // レスポンスの内容をストリームとして取得  
         using Stream content = await response.Content.ReadAsStreamAsync();
 
-        // 天気予報を取得
+        // 天気予報を取得  
         using JsonDocument jsonDocument = await JsonDocument.ParseAsync(content);
 
         string title = jsonDocument.RootElement
